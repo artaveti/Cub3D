1c0e11b710a8ec635186616f269e114c00e2f866
Artur Avetisyan <93.artur.avetisyan@gmail.com>
2024-04-26 14:19:56 +0400

    finished (from repository of 42 school account)

--- Cub3D/sources/addition_functions_five.c
+++
@@ -1,9 +0,0 @@
-#include "lib_for_cub3D.h"
-
-unsigned int	create_trgb(char *t, char *r, char *g, char *b)
-{
-	unsigned int result;
-
-	result = simple_atoi(t) << 24 | simple_atoi(r) << 16 | simple_atoi(g) << 8 | simple_atoi(b);
-	return (result);
-}
